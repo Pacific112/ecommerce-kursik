92b1759cea58e8da3dc91cb4b42086894818deeb
maciej.koziara <maciej.koziara@playboycontractors.com>
2023-09-17 09:03:09 +0200

    [chore] use h3 for product name

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const ProductDescription = ({ product: { name, price }, className }: Props) => (
 	<div className={clsx("text-black", className)}>
-		<div className="text-sm">{name}</div>
+		<h3 className="text-sm">{name}</h3>
 		<div className="text-xs font-bold">{formatPrice(price)}</div>
 	</div>
 );
