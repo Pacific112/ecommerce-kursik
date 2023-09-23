98980e67dd11395d65fe0a6042a81cfebefd7fd3
maciej.koziara <maciej.koziara@playboycontractors.com>
2023-09-23 07:33:06 +0200

    [feat] Add support for exact match

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -4,22 +4,31 @@ import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import { type Route } from "next";
+import { useMemo } from "react";
 
 export type ActiveLinkProps<T extends string> = {
 	href: Route<T>;
 	children: React.ReactNode;
 	activeClassName: string;
 	className: string;
+	exact?: boolean;
 };
 
 export function ActiveLink<T extends string>({
 	children,
 	href,
+	exact,
 	activeClassName,
 	className,
 }: ActiveLinkProps<T>) {
 	const pathname = usePathname();
-	const isActive = pathname === href;
+	const isActive = useMemo(() => {
+		if (exact || href === "/") {
+			return pathname === href;
+		}
+
+		return pathname.startsWith(href);
+	}, [pathname, href, exact]);
 
 	return (
 		<Link
